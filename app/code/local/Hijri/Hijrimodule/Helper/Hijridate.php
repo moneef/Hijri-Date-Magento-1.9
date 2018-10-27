930ee0f67e691d6362a85f4746bef4be3cd96f81
moneef <moneef@outlook.com>
2018-10-27 18:48:47 +0300

    fix fatal error cannot redeclare function

--- app/code/local/Hijri/Hijrimodule/Helper/Hijridate.php
+++ app/code/local/Hijri/Hijrimodule/Helper/Hijridate.php
@@ -2,57 +2,77 @@
 class Hijri_Hijrimodule_Helper_Hijridate extends Mage_Core_Helper_Abstract
 
 {
-	public
-
-	function Greg2Hijri($day, $month, $year, $string = false)
-	{
-		function intPart($float)
+		public	function intPart($float)
 		{
 			if ($float < - 0.0000001) return ceil($float - 0.0000001);
 			else return floor($float + 0.0000001);
 		}
 
-		function MonthName($month)
+		public function MonthName($month)
 		{
+			
 			$month = (int)$month;
-			static $months = array(
-				"Muharram",
-				"Safar",
-				"Rabee al-Awwal",
-				"Rabee al-Akhar",
-				"Jumada al-Awwal",
-				"Jumada al-Akhar",
-				"Rajab",
-				"Shaban",
-				"Ramadan",
-				"Shawwal",
-				"Dhu al-Qa'dah",
-				"Dhu al-Hijjah"
-			);
+			$locale = Mage::app()->getLocale()->getLocaleCode();
+			if ($locale === 'ar_SA') {
+				$months = array(
+					"محرم",
+					"صفر",
+					"ربيع الأول",
+					"ربيع الآخر",
+					"جمادى الأولى",
+					"جمادى الآخرة",
+					"رجب",
+					"شعبان",
+					"رمضان",
+					"شوال",
+					"ذو القعدة",
+					"ذو الحجة"
+				);
+			} else {
+				 $months = array(
+					"Muharram",
+					"Safar",
+					"Rabee al-Awwal",
+					"Rabee al-Akhar",
+					"Jumada al-Awwal",
+					"Jumada al-Akhar",
+					"Rajab",
+					"Shaban",
+					"Ramadan",
+					"Shawwal",
+					"Dhu al-Qa'dah",
+					"Dhu al-Hijjah"
+				);
+			}
+			
 			return $months[$month];
 		}
+		
+	public function Greg2Hijri($day, $month, $year, $string = false)
+	{
+
 
 		$day = (int)$day;
 		$month = (int)$month;
 		$year = (int)$year;
 		if (($year > 1582) or (($year == 1582) and ($month > 10)) or (($year == 1582) and ($month == 10) and ($day > 14)))
 		{
-			$jd = intPart((1461 * ($year + 4800 + intPart(($month - 14) / 12))) / 4) + intPart((367 * ($month - 2 - 12 * (intPart(($month - 14) / 12)))) / 12) - intPart((3 * (intPart(($year + 4900 + intPart(($month - 14) / 12)) / 100))) / 4) + $day - 32075;
+			$jd = $this->intPart((1461 * ($year + 4800 + $this->intPart(($month - 14) / 12))) / 4) + $this->intPart((367 * ($month - 2 - 12 * ($this->intPart(($month - 14) / 12)))) / 12) - $this->intPart((3 * ($this->intPart(($year + 4900 + $this->intPart(($month - 14) / 12)) / 100))) / 4) + $day - 32075;
 		}
 		else
 		{
-			$jd = 367 * $year - intPart((7 * ($year + 5001 + intPart(($month - 9) / 7))) / 4) + intPart((275 * $month) / 9) + $day + 1729777;
+			$jd = 367 * $year - $this->intPart((7 * ($year + 5001 + $this->intPart(($month - 9) / 7))) / 4) + $this->intPart((275 * $month) / 9) + $day + 1729777;
 		}
 
 		$l = $jd - 1948440 + 10632;
-		$n = intPart(($l - 1) / 10631);
+		$n = $this->intPart(($l - 1) / 10631);
 		$l = $l - 10631 * $n + 354;
-		$j = (intPart((10985 - $l) / 5316)) * (intPart((50 * $l) / 17719)) + (intPart($l / 5670)) * (intPart((43 * $l) / 15238));
-		$l = $l - (intPart((30 - $j) / 15)) * (intPart((17719 * $j) / 50)) - (intPart($j / 16)) * (intPart((15238 * $j) / 43)) + 29;
-		$month = intPart((24 * $l) / 709);
-		$day = $l - intPart((709 * $month) / 24);
+		$j = ($this->intPart((10985 - $l) / 5316)) * ($this->intPart((50 * $l) / 17719)) + ($this->intPart($l / 5670)) * ($this->intPart((43 * $l) / 15238));
+		$l = $l - ($this->intPart((30 - $j) / 15)) * ($this->intPart((17719 * $j) / 50)) - ($this->intPart($j / 16)) * ($this->intPart((15238 * $j) / 43)) + 29;
+		$month = $this->intPart((24 * $l) / 709);
+		$day = $l - $this->intPart((709 * $month) / 24);
 		$year = 30 * $n + $j - 30;
-		$month = MonthName($month - 1);
+		$month = $this->MonthName($month - 1);
 		$date = array();
 		$date['year'] = $year;
 		$date['month'] = $month;
